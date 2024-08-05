922a06701c6024254cfc0e9bcedab3e57b0206a0
M4riama <Mariamabdiallo1229@gmail.com>
2024-08-05 13:29:51 +0000

    Added Javascript and wrote a mini app to check peoples age

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log('Hello World!')
+
+// // Declare a variable and assign it a prompt
+
+// var prompt = prompt('What is your name?')
+
+// console.log(prompt)
+
+// if (10 > 9){
+//  console.log('Yes 10 is greater than 9');
+// } else{
+//   console.log('This  is false')
+// }
+
+var age = prompt('What is your age');
+if (age <  18) {
+alert ('You are a minor'); 
+} else if (age >= 18 && age < 60) {
+alert('You are an adult')
+} else {
+alert('damn you old')
+}
